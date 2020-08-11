b6d025375991ff2335e71fc4cfd10ea388a3a7b5
Sohanuzzaman Soad <mdsoad@gmail.com>
2020-08-12 02:26:10 +0600

    Init Commits

--- OnlineQuizPlatform/asgi.py
+++ OnlineQuizPlatform/asgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineQuizPlatform.settings')
