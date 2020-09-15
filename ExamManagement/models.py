8ba4084383c2111b96a17c99a49bd16df971b34d
Sohanuzzaman Soad <mdsoad@gmail.com>
2020-09-16 00:28:27 +0600

    ExamManagement Models Modified

--- ExamManagement/models.py
+++ ExamManagement/models.py
@@ -9,9 +9,9 @@ class Exam(models.Model):
     examiner_id = models.ForeignKey(Examiner, on_delete=models.SET_NULL, null=True, default=1)
     exam_code = models.IntegerField()
     exam_title = models.CharField(max_length=200)
-    exam_marks = models.IntegerField()
+    exam_marks = models.IntegerField(null=False, default=100)
     exam_date_time = models.DateTimeField()
-    exam_duration = models.CharField(max_length=20)
+    exam_duration = models.IntegerField(null=False, default=60)
 
     def __str__(self):
         return self.exam_title
