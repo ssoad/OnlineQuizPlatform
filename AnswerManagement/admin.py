921ed4583e23acf2c3005043bb40527fb84294e5
Mahady Al Hady <mahady426@gmail.com>
2020-09-16 16:43:21 +0600

    Examinee Answer added

--- AnswerManagement/admin.py
+++ AnswerManagement/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Answer
+from .models import Answer, ExamineeAnswer
 
 # Register your models here.
 
-admin.site.register([Answer])
+admin.site.register([Answer, ExamineeAnswer])
--- AnswerManagement/models.py
+++ AnswerManagement/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from ExamManagement.models import MCQ_Questions
+from ExamManagement.models import MCQ_Questions, Custom_Questions
+from Accounts.models import Examinee
 
 # Create your models here.
 
@@ -10,3 +11,11 @@ class Answer(models.Model):
     def __str__(self):
         return str(self.id)+".Answer of question:"+str(self.question.question_text)+str("("+self.question.question.exam.exam_title+")")
 
+class ExamineeAnswer(models.Model):
+    submitted_option = models.CharField(max_length=200, blank=True, null= True)
+    custom_answer = models.CharField(max_length=1000, blank=True, null= True)
+    mcq_question = models.ForeignKey(MCQ_Questions, on_delete=models.SET_NULL, null=True, default=1)
+    custom_question = models.ForeignKey(Custom_Questions, on_delete=models.SET_NULL, null=True, default=1)
+    examinee = models.ForeignKey(Examinee, on_delete=models.CASCADE, default=1)
+    def __str__(self):
+        return str(self.id)+".Answer Script:"+str(self.examinee.user.username)
