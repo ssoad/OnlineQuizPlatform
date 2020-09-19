fd20b838c38776c7e63cb637f3681df4fb331a16
Sohanuzzaman Soad <mdsoad@gmail.com>
2020-09-20 01:54:06 +0600

    Models(str) Modified & Froms Fixed

--- Accounts/models.py
+++ Accounts/models.py
@@ -2,14 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-
 # Create your models here.
 class Examinee(models.Model):
     organization = models.CharField(max_length=30, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.username
+        return str(self.user.first_name + " " + self.user.last_name)
 
 
 class Examiner(models.Model):
@@ -17,4 +16,4 @@ class Examiner(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.username
+        return str(self.user.first_name + " " + self.user.last_name)
