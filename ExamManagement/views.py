7af7cc32f42ba413078ebea6a7382fa41267feb5
Sohanuzzaman Soad <mdsoad@gmail.com>
2020-09-21 03:39:03 +0600

    AddExam Views Added with Auth Check

--- ExamManagement/views.py
+++ ExamManagement/views.py
@@ -1,6 +1,8 @@
+import datetime
 from django.shortcuts import render
-
-from .models import Exam,AttemptedExam,Question,CustomQuestion,MCQQuestion
+from Accounts.models import Examiner
+from .forms import AddExamForm
+from .models import Exam, AttemptedExam, Question, CustomQuestion, MCQQuestion
 
 
 # Create your views here.
@@ -16,48 +18,67 @@ def showExams(request):
 
 
 def showAttemptedExam(request):
-
     att_exams = AttemptedExam.objects.all()
 
     context = {
         'all_att_exams': att_exams
     }
 
-    return render(request, 'ExamManagement/showAttemptedExam.html',context)
+    return render(request, 'ExamManagement/showAttemptedExam.html', context)
 
 
 def showQuestions(request):
-
     questions = Question.objects.all()
 
-    context={
-        'all_questions' : questions
+    context = {
+        'all_questions': questions
     }
 
-    return render(request, 'ExamManagement/showQuestion.html',context)
+    return render(request, 'ExamManagement/showQuestion.html', context)
 
 
 def showMCQQuestions(request):
-
     MCQquestions = MCQQuestion.objects.all()
 
     context = {
-        'all_mcqquestions' : MCQquestions
+        'all_mcqquestions': MCQquestions
     }
 
-    return render(request, 'ExamManagement/showMCQQuestions.html',context)
+    return render(request, 'ExamManagement/showMCQQuestions.html', context)
 
 
 def showCustomQuestions(request):
-
     customquestion = CustomQuestion.objects.all()
 
     context = {
-        'all_customquestions' : customquestion
+        'all_customquestions': customquestion
     }
 
-    return render(request, 'ExamManagement/showCustomQuestion.html',context)
+    return render(request, 'ExamManagement/showCustomQuestion.html', context)
 
 
+def addExam(request):
+    if request.user.is_authenticated:
+        examiner = Examiner.objects.filter(user_id=request.user.id)
 
-
+        #print('TEST:',examiner[0])
+        if examiner:
+            form = AddExamForm(request.POST or None)
+            if request.method == 'POST':
+                if form.is_valid():
+                    # form.save()
+                    title = form.cleaned_data.get('exam_title')
+                    code = int(form.cleaned_data.get('exam_code'))
+                    marks = int(form.cleaned_data.get('exam_marks'))
+                    date_time = datetime.datetime.now() #For Testing purpose
+                    duration = int(form.cleaned_data.get('exam_duration'))
+                    exam = Exam(exam_title=title, examiner=examiner[0], exam_code=code, exam_marks=marks, exam_duration=duration, exam_date_time=date_time)
+                    exam.save()
+                    form = AddExamForm()
+            context = {
+                'form': form
+            }
+            return render(request, 'ExamManagement/addExam.html', context)
+        else:
+            return render(request, '404.html')
+    return render(request, '404.html')
