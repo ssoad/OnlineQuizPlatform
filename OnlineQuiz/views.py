4813d740b93ff7aea1de6786ec8e7a838aca23a6
Sohanuzzaman Soad <mdsoad@gmail.com>
2020-10-01 01:52:36 +0600

    Bug Fixed (Navbar Conflict Issue)

--- OnlineQuiz/views.py
+++ OnlineQuiz/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from Accounts.models import  Examinee, Examiner
 import django.contrib.staticfiles
 
 # Create your views here.
 def showHome(request):
+    examinee = Examinee.objects.filter(user=request.user)
+    examiner = Examiner.objects.filter(user=request.user)
+    if examinee:
+        context = {
+            'examinee':True
+        }
+        return render(request, 'Index.html', context)
+    elif examiner:
+        context = {
+            'examiner': True
+        }
+        return render(request, 'Index.html', context)
     return render(request, 'Index.html')
 
 
