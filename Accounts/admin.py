70def1cfbaf9dcc5babb2b9040b6d428feff8f6f
Sohanuzzaman Soad <mdsoad@gmail.com>
2020-10-02 21:18:52 +0600

    Modify for Verifying Process

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Examinee,Examiner, Profile
+from .models import Examinee, Examiner, Profile
 
 # Register your models here.
 admin.site.register([Examinee, Examiner, Profile])
