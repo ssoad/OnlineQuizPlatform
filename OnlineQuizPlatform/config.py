c29b4ad3f58852007a8cbd66f07ef8bc963b3ec0
Sohanuzzaman Soad <mdsoad@gmail.com>
2021-04-03 23:41:19 +0600

    Update config.py

--- OnlineQuizPlatform/config.py
+++ OnlineQuizPlatform/config.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
+
 EMAIL_ACC = "onlinequizplatfom@gmail.com"
 PASSWORD = "@Cse2020"
-=======
-EMAIL_ACC = "onlinequizplatfom@gmail.com" #User Your Own Email
-PASSWORD = "@Cse2020" #Enter Password
->>>>>>> 6b91886cbdc723847ed9d09446e898c8e174bc22
